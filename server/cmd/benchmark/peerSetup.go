7f4c5f01f46d5b5b8a594934973d51abfb783a47
Abdu-Rauf <abdurraufaamir@gmail.com>
2026-02-14 14:21:27 +0530

    Writing Metrics to csv file

--- server/cmd/benchmark/peerSetup.go
+++ server/cmd/benchmark/peerSetup.go
@@ -2,11 +2,16 @@ package main
 
 import (
     "log"
-
+    "encoding/json"
     "github.com/gorilla/websocket"
     "github.com/pion/webrtc/v3"
 	"sync"
+    "time"
+    "os"
+    "fmt"
+    
 )
+var once sync.Once
 
 type PeerSession struct {
     PeerConnection *webrtc.PeerConnection
@@ -14,7 +19,7 @@ type PeerSession struct {
 	WsMu           sync.Mutex
 }
 
-func PeerSetup(conn *websocket.Conn) (*PeerSession, error) {
+func PeerSetup(conn *websocket.Conn ,csvFile *os.File) (*PeerSession, error) {
     pc, err := webrtc.NewPeerConnection(webrtc.Configuration{})
     if err != nil {
         return nil, err
@@ -34,17 +39,48 @@ func PeerSetup(conn *websocket.Conn) (*PeerSession, error) {
         })
 
         dc.OnMessage(func(msg webrtc.DataChannelMessage) {
-            log.Println("Received:", string(msg.Data))
+            // log.Println("Received:", string(msg.Data))
+
+            var resp struct {
+                Type string `json:"type"`
+                Latency int64 `json:"latency"`
+                Seq int `json:"seq"`
+                Ts int64 `json:"ts"`
+            }
+            json.Unmarshal(msg.Data, &resp)
+
+            if resp.Type == "benchmark_ack"{
+                now := time.Now().UnixMicro()
+                totalDrift:= now-resp.Ts
+
+                // Append row in the file
+                fmt.Fprintf(csvFile, "%f,%d,%.2f,%.2f\n", 
+                    float64(now)/1000000.0, 
+                    resp.Seq, 
+                    float64(resp.Latency)/1000.0, 
+                    float64(totalDrift)/1000.0)
+            } 
+
+        })
+        dc.OnClose(func() {
+            log.Println("Data channel closed")
         })
     })
 
     // Consume video packets to keep connection alive
     pc.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
-        log.Println("Receiving video track:", track.Codec().MimeType)
+        
         buf := make([]byte, 1500)
+        _, _, err := track.Read(buf)
+        if err != nil {
+            return
+        }
+        once.Do(func() {
+            log.Println("First packet received. Pipeline is hot.")
+            close(videoStarted)
+        })
         for {
-            _, _, err := track.Read(buf)
-            if err != nil {
+            if _, _, err := track.Read(buf); err != nil {
                 return
             }
         }
