a072ccde3f19bb16be6f1b486a4cde06ccb12cae
Abdu-Rauf <abdurraufaamir@gmail.com>
2026-04-10 19:28:50 +0530

    Add LogFile cleanup

--- server/cleanup.go
+++ server/cleanup.go
@@ -1,46 +1,53 @@
 package main
 
-import(
+import (
 	"context"
 	"log"
-	"github.com/pion/webrtc/v3"
-	"github.com/docker/docker/client"
 
+	"github.com/docker/docker/client"
+	"github.com/pion/webrtc/v3"
 )
 
-
 func Cleanup(
-    cancelStream context.CancelFunc,
-    container *Container,
-    dockerCli *client.Client,
-    pc *webrtc.PeerConnection,
+	cancelStream context.CancelFunc,
+	container *Container,
+	dockerCli *client.Client,
+	pc *webrtc.PeerConnection,
 ) {
 	// Cancel video streaming
-	if cancelStream!=nil{
+	if cancelStream != nil {
 		log.Println("Closing Video Stream")
 		cancelStream()
 	}
 
-	if pc!=nil{
+	if pc != nil {
 		pc.Close()
 	}
 
-	// save session info 
-	if container!=nil{
+	// save session info
+	if container != nil {
 		log.Println("Save session history")
 	}
 	if container != nil {
-		if container.Conn != nil  {
+		// Close The writer After writing In the LogFile
+		if container.csvWriter != nil {
+			container.csvWriter.Flush()
+		}
+		// Close the Logfile
+		if container.logFile != nil {
+			container.logFile.Close()
+			log.Println("CSV Log file saved and closed securely.")
+		}
+		if container.Conn != nil {
 			log.Println("Closing the videoStream connections")
 			container.Conn.Close()
 		}
-		if container.InputConn !=nil{
+		if container.InputConn != nil {
 			log.Println("Closing the Input Connection")
 			container.InputConn.Close()
 		}
-		RemoveContainer(context.Background(),dockerCli,container.ID)
+		RemoveContainer(context.Background(), dockerCli, container.ID)
 		log.Println("Completed Cleanup")
 	}
-
 
 }
