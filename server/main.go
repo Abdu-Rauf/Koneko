7378aa3a763ddacab8f04c5c8c7209e519ec18d4
Abdu-Rauf <abdurraufaamir@gmail.com>
2026-04-10 19:30:01 +0530

    Invoke Logging Cursor Path

--- server/main.go
+++ server/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
-	"sync"	
+	"sync"
+	"time"
+
+	"github.com/docker/docker/client"
 	"github.com/gorilla/websocket"
 	"github.com/pion/webrtc/v3"
-	"context"
-	"time"
-	"github.com/docker/docker/client"
 )
-
 
 func CheckDomain(r *http.Request) bool {
 	// allow only koneko's domain
@@ -30,8 +30,8 @@ func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
 	var closeOnce sync.Once
 
 	// Connect for signalling
-	conn,err:= SignalSocket(w,r)
-	if err!=nil{
+	conn, err := SignalSocket(w, r)
+	if err != nil {
 		log.Println("Error in establishing ws connection")
 		return
 	}
@@ -46,17 +46,16 @@ func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
 	var container *Container
 	var cancelStream context.CancelFunc
 
-	var browserMsg struct{
-		Type string `json:"type"`
+	var browserMsg struct {
+		Type    string `json:"type"`
 		Browser string `json:"browser"`
 	}
 	err = conn.ReadJSON(&browserMsg)
-	if err!=nil{
+	if err != nil {
 		log.Println("Error reading browser selected")
 		return
 	}
-	log.Println("Browser selected:" ,browserMsg.Browser)
-
+	log.Println("Browser selected:", browserMsg.Browser)
 
 	// Add video Track before creating Peer
 	videoTrack, err := webrtc.NewTrackLocalStaticSample(
@@ -70,10 +69,10 @@ func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Start Container Setup In a Routine
-	go func(){
+	go func() {
 		defer close(containerReady)
-		c,cs,err := ContainerSetup(videoTrack,browserMsg.Browser,s.DockerCli)
-		if err!=nil{
+		c, cs, err := ContainerSetup(videoTrack, browserMsg.Browser, s.DockerCli)
+		if err != nil {
 			log.Println("Error Setting Up Container")
 			conn.Close()
 			return
@@ -82,15 +81,14 @@ func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
 		cancelStream = cs
 	}()
 
-
 	// Setup Peer Before Signalling and Create DataChannel
-	peer, err := PeerSetup(conn,videoTrack,connectionClosed,&closeOnce)
-	if err!=nil{
+	peer, err := PeerSetup(conn, videoTrack, connectionClosed, &closeOnce)
+	if err != nil {
 		log.Println("Error setting Up peers")
 		conn.Close()
 		return
 	}
-	
+
 	// Attach DataChannel Listeners For User Input Forwarding
 	AttachDcListeners(
 		containerReady,
@@ -100,10 +98,9 @@ func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
 		connectionClosed,
 		&closeOnce,
 	)
-	
 
 	// Read messages from websocket
-	go Signalling(conn,peer.PC)
+	go Signalling(conn, peer.PC)
 
 	// Wait for dc to be ready then close the ws
 	<-dataChannelReady
@@ -111,27 +108,33 @@ func (s *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(200 * time.Millisecond)
 	conn.Close()
 
+	// Start Logging Cursor movements
+	err = container.StartLogging("./cmd/dataset/normal_browse.csv")
+	if err != nil {
+		log.Println("Skipping logging:", err)
+	}
+
 	// Wait till connection is closed by client
 	<-connectionClosed
 	log.Println("Connection closed , clean up")
 
 	// Cleanup
-	Cleanup(cancelStream,container,s.DockerCli,peer.PC)
+	Cleanup(cancelStream, container, s.DockerCli, peer.PC)
 
 }
 
 func main() {
-	cli,err := client.NewClientWithOpts(client.FromEnv,client.WithAPIVersionNegotiation())
-	if err!=nil{
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
 		log.Println("Failed To Create Docker Cli")
 	}
 	defer cli.Close()
 	myServer := &Server{
-		DockerCli:cli,
+		DockerCli: cli,
 	}
-	
+
 	fs := http.FileServer(http.Dir("../client"))
-    http.Handle("/", fs)
+	http.Handle("/", fs)
 
 	http.HandleFunc("/ws", myServer.wsHandler)
 	http.ListenAndServe(":8080", nil)
